ce1c6f201a5f3804346672f619591c58355597fa
paraj1305 <bhatasanaparaj@gmail.com>
2024-08-06 22:57:36 +0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stock_analyze_project.wsgi:application
--- stock_analyze_project/settings.py
+++ stock_analyze_project/settings.py
@@ -140,3 +140,17 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+import os
+
+# Static files settings
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Add these lines at the bottom of the file
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
